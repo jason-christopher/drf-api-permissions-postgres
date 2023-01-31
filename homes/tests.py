9cb22b188b44f95e3157f99ff7d15da92a087a18
Jason Christopher <jchristopher2448@gmail.com>
2023-01-31 16:47:06 -0600

    completed CC32, tests passing

--- homes/tests.py
+++ homes/tests.py
@@ -2,11 +2,10 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from .models import Home
 
-from .models import Thing
 
-
-class ThingTests(APITestCase):
+class HomeTests(APITestCase):
     # In Python, the @classmethod decorator is used to declare a method in the class as a class method that can be called using ClassName.MethodName()
     # click the blue circle, this overrides a particular method
     @classmethod
@@ -16,76 +15,85 @@ class ThingTests(APITestCase):
         )
         testuser1.save()
 
-        test_thing = Thing.objects.create(
-            name="rake",
+        test_home = Home.objects.create(
+            street_address="1234 Fake St.",
             owner=testuser1,
-            description="Better for collecting leaves than a shovel.",
+            city="Orlando",
+            state="FL",
+            description="Orlando house.",
         )
-        test_thing.save()
+        test_home.save()
 
-    # NEW
     def setUp(self):
         self.client.login(username="testuser1", password="pass")
 
-    def test_things_model(self):
-        thing = Thing.objects.get(id=1)
-        actual_owner = str(thing.owner)
-        actual_name = str(thing.name)
-        actual_description = str(thing.description)
+    def test_homes_model(self):
+        home = Home.objects.get(id=1)
+        actual_owner = str(home.owner)
+        actual_street_address = str(home.street_address)
+        actual_description = str(home.description)
         self.assertEqual(actual_owner, "testuser1")
-        self.assertEqual(actual_name, "rake")
+        self.assertEqual(actual_street_address, "1234 Fake St.")
         self.assertEqual(
-            actual_description, "Better for collecting leaves than a shovel."
+            actual_description, "Orlando house."
         )
 
-    def test_get_thing_list(self):
-        url = reverse("thing_list")
+    def test_get_home_list(self):
+        url = reverse("home_list")
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        things = response.data
-        self.assertEqual(len(things), 1)
-        self.assertEqual(things[0]["name"], "rake")
+        homes = response.data
+        self.assertEqual(len(homes), 1)
+        self.assertEqual(homes[0]["street_address"], "1234 Fake St.")
 
-    def test_get_thing_by_id(self):
-        url = reverse("thing_detail", args=(1,))
+    def test_get_home_by_id(self):
+        url = reverse("home_detail", args=(1,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        thing = response.data
-        self.assertEqual(thing["name"], "rake")
+        home = response.data
+        self.assertEqual(home["street_address"], "1234 Fake St.")
 
-    def test_create_thing(self):
-        url = reverse("thing_list")
-        data = {"owner": 1, "name": "spoon", "description": "good for cereal and soup"}
-        response = self.client.post(url, data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        things = Thing.objects.all()
-        self.assertEqual(len(things), 2)
-        self.assertEqual(Thing.objects.get(id=2).name, "spoon")
-
-    def test_update_thing(self):
-        url = reverse("thing_detail", args=(1,))
+    def test_create_home(self):
+        url = reverse("home_list")
         data = {
             "owner": 1,
-            "name": "rake",
-            "description": "pole with a crossbar toothed like a comb.",
+            "street_address": "2345 Test Ave.",
+            "city": "Seattle",
+            "state": "WA",
+            "description": "Seattle house."
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        homes = Home.objects.all()
+        self.assertEqual(len(homes), 2)
+        self.assertEqual(Home.objects.get(id=2).street_address, "2345 Test Ave.")
+
+    def test_update_home(self):
+        url = reverse("home_detail", args=(1,))
+        data = {
+            "owner": 1,
+            "street_address": "3456 Update Ave.",
+            "city": "Austin",
+            "state": "TX",
+            "description": "Austin house."
         }
         response = self.client.put(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        thing = Thing.objects.get(id=1)
-        self.assertEqual(thing.name, data["name"])
-        self.assertEqual(thing.owner.id, data["owner"])
-        self.assertEqual(thing.description, data["description"])
+        home = Home.objects.get(id=1)
+        self.assertEqual(home.street_address, data["street_address"])
+        self.assertEqual(home.owner.id, data["owner"])
+        self.assertEqual(home.description, data["description"])
 
-    def test_delete_thing(self):
-        url = reverse("thing_detail", args=(1,))
+    def test_delete_home(self):
+        url = reverse("home_detail", args=(1,))
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        things = Thing.objects.all()
-        self.assertEqual(len(things), 0)
+        homes = Home.objects.all()
+        self.assertEqual(len(homes), 0)
 
     # New
     def test_authentication_required(self):
         self.client.logout()
-        url = reverse("thing_list")
+        url = reverse("home_list")
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
